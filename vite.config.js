2144539c4b8b99e257552836a370b182b1a7d537
fwenhua <fwenhua@qualicom.com.cn>
2024-10-16 11:34:40 +0800

    * update

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,16 @@ import { defineConfig, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
 
-import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
+import cdn from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const { VITE_BASE_PATH } = loadEnv(mode, process.cwd());
   return {
     plugins: [vue(),
-    importToCDN({
-        modules: [
-            {
-                name: 'vue-demi',
-                var: 'VueDemi',
-                path: "lib/index.iife.min.js",
-            },
-            {
-                name: 'pinia',
-                var: 'Pinia',
-                path: 'dist/pinia.iife.min.js'
-            }
-        ],
-    }),],
+      cdn({
+        modules: ['vue'],
+    }),,],
     resolve: {
       // ↓路径别名，主要是这部分
       alias: {
