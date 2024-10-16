d152ac5243780aaadbfed4c4cb78b4b588fb1bdf
fwenhua <fwenhua@qualicom.com.cn>
2024-10-16 10:22:44 +0800

    + update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,33 @@ import { defineConfig, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
 
+import { Plugin as importToCND } from 'vite-plugin-cdn-import'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const { VITE_BASE_PATH } = loadEnv(mode, process.cwd());
   return {
-    plugins: [vue()],
+    plugins: [vue(),
+    // 使用CDN模块信息配置插件
+    importToCND({
+      modules: [
+          {
+              name: 'vue',
+              var: 'Vue',
+              path: 'https://cdn.jsdelivr.net/npm/vue@3.3.11/dist/vue.global.min.js',
+          },
+          {
+              name: 'vue-router',
+              var: 'VueRouter',
+              path: 'https://cdn.jsdelivr.net/npm/vue-router@4.2.5/dist/vue-router.global.min.js',
+          },
+          {
+              name: 'vue-demi',
+              var: 'VueDemi',
+              path: 'https://cdn.jsdelivr.net/npm/vue-demi@0.14.6/lib/index.iife.min.js',
+          }
+      ]
+  })],
     resolve: {
       // ↓路径别名，主要是这部分
       alias: {
