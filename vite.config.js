4dbf8e3c1fe95b1d0d6615eb9d950c6278cd85d9
fwenhua <fwenhua@qualicom.com.cn>
2024-10-16 11:13:07 +0800

    + update

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,27 @@ import { defineConfig, loadEnv } from "vite";
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
 
-import { Plugin as importToCND } from 'vite-plugin-cdn-import'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const { VITE_BASE_PATH } = loadEnv(mode, process.cwd());
   return {
     plugins: [vue(),
-    // 使用CDN模块信息配置插件
-    importToCND({
-      modules: [
-          {
-              name: 'vue',
-              var: 'Vue',
-              path: 'https://cdn.jsdelivr.net/npm/vue@3.3.11/dist/vue.global.min.js',
-          },
-          {
-              name: 'vue-router',
-              var: 'VueRouter',
-              path: 'https://cdn.jsdelivr.net/npm/vue-router@4.2.5/dist/vue-router.global.min.js',
-          },
-          {
-              name: 'vue-demi',
-              var: 'VueDemi',
-              path: 'https://cdn.jsdelivr.net/npm/vue-demi@0.14.6/lib/index.iife.min.js',
-          }
-      ]
-  })],
+    importToCDN({
+        modules: [
+            {
+                name: 'vue-demi',
+                var: 'VueDemi',
+                path: "lib/index.iife.min.js",
+            },
+            {
+                name: 'pinia',
+                var: 'Pinia',
+                path: 'dist/pinia.iife.min.js'
+            }
+        ],
+    }),],
     resolve: {
       // ↓路径别名，主要是这部分
       alias: {
